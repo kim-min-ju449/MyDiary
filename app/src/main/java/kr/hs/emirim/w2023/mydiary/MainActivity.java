9159354e2c12bb121e5daad8878b62a2bbe8714a
김민주 <w2023@e-mirim.hs.kr>
2021-07-31 13:45:32 +0900

    Main_Acvtivity Commit

--- app/src/main/java/kr/hs/emirim/w2023/mydiary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2023/mydiary/MainActivity.java
@@ -1,14 +1,53 @@
 package kr.hs.emirim.w2023.mydiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mMianRecycleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMianRecycleView = findViewById(R.id.main_recycle_view);
+        findViewById(R.id.main_write_button).setOnClickListener(this);
+        //41:25
+
+    }
+    @Override
+    public void OnClick(View v){
+
+    }
+
+    private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        class MainViewHolder extends RecyclerView.ViewHolder{
+            public MainViewHolder(View itemView){
+                super(itemView);
+            }
+        }
     }
 }
