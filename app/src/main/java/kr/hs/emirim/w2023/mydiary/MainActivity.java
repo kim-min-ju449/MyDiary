476d68aadf7bf12a2a4d9542e6b8bba3ea64825c
김민주 <w2023@e-mirim.hs.kr>
2021-07-31 14:16:01 +0900

    작동까지 성공!!!

--- app/src/main/java/kr/hs/emirim/w2023/mydiary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2023/mydiary/MainActivity.java
@@ -8,9 +8,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mMianRecycleView;
+    private MainAdapter mAdapter;
+    private List<Board> mBoardList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMianRecycleView = findViewById(R.id.main_recycle_view);
-        findViewById(R.id.main_write_button).setOnClickListener(this);
-        //41:25
+       // findViewById(R.id.main_write_button).setOnClickListener(this);
+
+        mBoardList = new ArrayList<>();
+        mBoardList.add(new Board(null,"fff",null));
+        mBoardList.add(new Board(null,"fff",null));
+        mBoardList.add(new Board(null,"ㅋㅋㅋㅋ",null));
+        mBoardList.add(new Board(null,"OK",null));
+        mBoardList.add(new Board(null,"반갑다",null));
+
+        mAdapter = new MainAdapter(mBoardList);
+        mMianRecycleView.setAdapter(mAdapter);
+
 
     }
-    @Override
-    public void OnClick(View v){
-
-    }
+//    @Override
+//    public void OnClick(View v){
+//
+//    }
 
     private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+
+        private List<Board> mBoardList;
+        public MainAdapter(List<Board> mBoardList){
+            this.mBoardList=mBoardList;
+        }
         @NonNull
         @Override
         public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -36,17 +57,26 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+            Board data = mBoardList.get(position);
+            holder.mtitleTextView.setText(data.getTitle());
+            holder.mNameTextView.setText(data.getName());
 
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mBoardList.size();
         }
 
         class MainViewHolder extends RecyclerView.ViewHolder{
+
+            private TextView mtitleTextView;
+            private TextView mNameTextView;
             public MainViewHolder(View itemView){
                 super(itemView);
+
+                mtitleTextView = itemView.findViewById(R.id.item_title_text);
+                mNameTextView = itemView.findViewById(R.id.item_name_list);
             }
         }
     }
